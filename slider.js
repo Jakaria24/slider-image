21ae577037d7721d7c16de8ebf9a141829328883
Jakaria24 <Jisanmahmud277720@gmail.com>
2021-11-03 15:33:14 +0600

    slider image fisrt section complete

---
+++ slider.js
@@ -0,0 +1,19 @@
+const images =[
+    'images/img-1.jpg',
+    'images/img-2.jpg',
+    'images/img-3.jpg',
+    'images/img-4.jpg',
+    'images/img-5.jpg',
+    'images/img-6.jpg',
+]
+let imgIndex =0;
+const imgElement = document.getElementById('slider-img')
+setInterval(()=>{
+    if(imgIndex >= images.length){
+        imgIndex=0
+
+    }
+    const imgUrl = images[imgIndex]
+    imgElement.setAttribute('src',imgUrl);
+    imgIndex++
+},2000)
